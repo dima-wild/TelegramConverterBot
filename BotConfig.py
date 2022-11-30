b2facf866cf223e8653273ec7eb6e92ca7774938
dima-wild <116372585+dima-wild@users.noreply.github.com>
2022-11-30 12:09:40 +0200

    Add files via upload

---
+++ BotConfig.py
@@ -0,0 +1,12 @@
+TOKEN = '5832614263:AAGbum8N9-oOaPW-g388XyVqCKkH-nHj8UY'
+APIKEY = "76ba8d43236bf73e072cc65f59734538e9c40e9a02cdbfec045128e6c8bb6063"
+keys = {
+    'рубль': 'RUB',
+    'доллар': 'USD',
+    'евро': 'EUR',
+    'юань': 'СNY',
+    'фунт': 'GBP',
+    'иена': 'JPY',
+    'лира': 'TRY',
+    'франк': 'CHF',
+}
